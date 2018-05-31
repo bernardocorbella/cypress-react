185e6b10afa8d67abc023b80a05707ada4f8ce43
Bernardo Corbella <corbellabernardo@gmail.com>
2018-05-31 20:26:52 -0300

    navigation tests

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -8,10 +8,28 @@ describe('<App />', () => {
   });
 
   context('<Nav />', () => {
+    let aboutLink, homeLink, loginLink;
+    beforeEach(() => {
+      aboutLink = cy.get('[data-testid=About-Link]');
+      homeLink = cy.get('[data-testid=Home-Link]');
+      loginLink = cy.get('[data-testid=Login-Link]');
+    });
+
     it('should have navigation', () => {
-      cy.get('[data-testid=About-Link]').should('contain', 'About');
-      cy.get('[data-testid=Home-Link]').should('contain', 'Home');
-      cy.get('[data-testid=Login-Link]').should('contain', 'Login');
+      aboutLink.should('contain', 'About');
+      homeLink.should('contain', 'Home');
+      loginLink.should('contain', 'Login');
+    });
+
+    it('should correctly navigate', () => {
+      loginLink.click();
+      expect(location.href).to.eq('/login');
+
+      aboutLink.click();
+      expect(location.href).to.eq('/about');
+
+      homeLink.click();
+      expect(location.href).to.eq('/');
     });
   });
 });
