0942e91562f86ba894e36a130ade7bea9a7b230a
Bernardo Corbella <corbellabernardo@gmail.com>
2018-05-31 19:55:13 -0300

    adds `Nav` items test

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -7,11 +7,9 @@ describe('<App />', () => {
     cy.title().should('include', 'Cypress React App');
   });
 
-  it('should navigate to about', () => {
-    const button = cy.get('[data-testid=AboutLink]');
-
-    button.should('contain', 'About');
-    button.click();
-    // expect(location.href).to.eq('/about');
+  it('should have navigation', () => {
+    cy.get('[data-testid=About-Link]').should('contain', 'About');
+    cy.get('[data-testid=Home-Link]').should('contain', 'Home');
+    cy.get('[data-testid=Login-Link]').should('contain', 'Login');
   });
 });
